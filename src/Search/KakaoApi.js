8a4f0b7c8c1fd4db57428aabed763f23cee9de7d
(yunjin) <sion11290805@gmail.com>
2021-09-14 20:46:32 +0900

    리액트로변환

--- src/Search/KakaoApi.js
+++ src/Search/KakaoApi.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const KakaoApi = ({searchedKeyword}) => {
+  console.log("KakaoApi")
   const { kakao } = window;
+
+  if(searchedKeyword){
+    searchPlaces();
+  }
 
   let ps = new kakao.maps.services.Places();  
 
+  // 키워드 검색을 요청하는 함수입니다
   function searchPlaces() {
+
     if (!searchedKeyword.replace(/^\s+|\s+$/g, '')) {
         alert('키워드를 입력해주세요!');
         return false;
@@ -30,6 +37,84 @@ const KakaoApi = ({searchedKeyword}) => {
         return;
     }
   }
+
+  // 검색 결과 목록과 마커를 표출하는 함수입니다
+  function displayPlaces(places) {
+
+    let listEl = document.getElementById('placesList');
+    
+    // 검색 결과 목록에 추가된 항목들을 제거합니다
+    removeAllChildNods(listEl);
+
+    for ( var i=0; i<places.length; i++ ) {
+        itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성합니다
+    // 검색결과 항목들을 검색결과 목록 Elemnet에 추가합니다
+        listEl.append(itemEl);
+    }
+  }
+
+  // 검색결과 항목을 Element로 반환하는 함수입니다
+  function getListItem(index, places) {
+
+    var el = document.createElement('li'),
+    itemStr = '<span class="markerbg marker_' + (index+1) + '"></span>' +
+                '<div class="info">' +
+                '   <a href="' + places.place_url + '">' + places.place_name + '</a>';
+
+    if (places.road_address_name) {
+        itemStr += '    <span>' + places.road_address_name + '</span>' +
+                    '   <span class="jibun gray">' +  places.address_name  + '</span>';
+    } else {
+        itemStr += '    <span>' +  places.address_name  + '</span>'; 
+    }
+                
+    itemStr += '  <span class="tel">' + places.phone  + '</span>' +
+                '</div>';           
+
+    el.innerHTML = itemStr;
+    el.className = 'item';
+
+    return el;
+  }
+
+
+// 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
+function displayPagination(pagination) {
+  let paginationEl = document.getElementById('pagination');
+  let i; 
+
+  // 기존에 추가된 페이지번호를 삭제합니다
+  while (paginationEl.hasChildNodes()) {
+      paginationEl.removeChild (paginationEl.lastChild);
+  }
+
+  for (i=1; i<=pagination.last; i++) {
+      var el = document.createElement('a');
+      el.href = "#";
+      el.innerHTML = i;
+
+      if (i===pagination.current) {
+          el.className = 'on';
+      } else {
+          el.onclick = (function(i) {
+              return function() {
+                  pagination.gotoPage(i);
+              }
+          })(i);
+      }
+
+      paginationEl.append(el);
+  }
+}
+
+
+  // 검색결과 목록의 자식 Element를 제거하는 함수입니다
+  function removeAllChildNods(el) {   
+    while (el.hasChildNodes()) {
+        el.removeChild (el.lastChild);
+    }
+  }
+
 
   return(
     <>
--- src/Search/SearchResult.js
+++ src/Search/SearchResult.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../css/SearchResult.css';
+import {Card } from 'react-bootstrap'
 
 const SearchResult = () => {
 
   return(
     <>
-
+      <Card className="searchResultCard" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Body>
+          <Card.Link href="#">Card title</Card.Link>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </>
   )
 }
