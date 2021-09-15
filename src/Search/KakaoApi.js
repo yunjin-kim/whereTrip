faaf0db7827244ad73aadc8b871a71ff9b9ccb01
(yunjin) <sion11290805@gmail.com>
2021-09-15 20:40:05 +0900

    키워드검색결과불러오기

--- src/Search/KakaoApi.js
+++ src/Search/KakaoApi.js
@@ -1,119 +1,41 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
+const { kakao } = window;
 
 const KakaoApi = ({searchedKeyword}) => {
   console.log("KakaoApi")
-  const { kakao } = window;
-
-  if(searchedKeyword){
-    searchPlaces();
-  }
-
   let ps = new kakao.maps.services.Places();  
 
-  // 키워드 검색을 요청하는 함수입니다
-  function searchPlaces() {
-
-    if (!searchedKeyword.replace(/^\s+|\s+$/g, '')) {
-        alert('키워드를 입력해주세요!');
-        return false;
-    }
-    // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
-    ps.keywordSearch( searchedKeyword, placesSearchCB); 
-  }
-
-  // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
-  function placesSearchCB(data, status, pagination) {
-    if (status === kakao.maps.services.Status.OK) {
-        // 정상적으로 검색이 완료됐으면
-        // 검색 목록과 마커를 표출합니다
-        displayPlaces(data);
-        // 페이지 번호를 표출합니다
-        displayPagination(pagination);
-    } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-        alert('검색 결과가 존재하지 않습니다.');
-        return;
-    } else if (status === kakao.maps.services.Status.ERROR) {
-        alert('검색 결과 중 오류가 발생했습니다.');
-        return;
-    }
-  }
-
-  // 검색 결과 목록과 마커를 표출하는 함수입니다
-  function displayPlaces(places) {
-
-    let listEl = document.getElementById('placesList');
+  useEffect(() => {
+    searchPlaces(searchedKeyword)
     
-    // 검색 결과 목록에 추가된 항목들을 제거합니다
-    removeAllChildNods(listEl);
 
-    for ( var i=0; i<places.length; i++ ) {
-        itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성합니다
-    // 검색결과 항목들을 검색결과 목록 Elemnet에 추가합니다
-        listEl.append(itemEl);
-    }
-  }
-
-  // 검색결과 항목을 Element로 반환하는 함수입니다
-  function getListItem(index, places) {
-
-    var el = document.createElement('li'),
-    itemStr = '<span class="markerbg marker_' + (index+1) + '"></span>' +
-                '<div class="info">' +
-                '   <a href="' + places.place_url + '">' + places.place_name + '</a>';
-
-    if (places.road_address_name) {
-        itemStr += '    <span>' + places.road_address_name + '</span>' +
-                    '   <span class="jibun gray">' +  places.address_name  + '</span>';
-    } else {
-        itemStr += '    <span>' +  places.address_name  + '</span>'; 
-    }
-                
-    itemStr += '  <span class="tel">' + places.phone  + '</span>' +
-                '</div>';           
-
-    el.innerHTML = itemStr;
-    el.className = 'item';
-
-    return el;
-  }
-
-
-// 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
-function displayPagination(pagination) {
-  let paginationEl = document.getElementById('pagination');
-  let i; 
-
-  // 기존에 추가된 페이지번호를 삭제합니다
-  while (paginationEl.hasChildNodes()) {
-      paginationEl.removeChild (paginationEl.lastChild);
-  }
-
-  for (i=1; i<=pagination.last; i++) {
-      var el = document.createElement('a');
-      el.href = "#";
-      el.innerHTML = i;
-
-      if (i===pagination.current) {
-          el.className = 'on';
-      } else {
-          el.onclick = (function(i) {
-              return function() {
-                  pagination.gotoPage(i);
-              }
-          })(i);
+    // 키워드 검색을 요청하는 함수입니다
+    function searchPlaces() {
+  
+      if (!searchedKeyword.replace(/^\s+|\s+$/g, '')) {
+          alert('키워드를 입력해주세요!');
+          return false;
       }
-
-      paginationEl.append(el);
-  }
-}
-
-
-  // 검색결과 목록의 자식 Element를 제거하는 함수입니다
-  function removeAllChildNods(el) {   
-    while (el.hasChildNodes()) {
-        el.removeChild (el.lastChild);
+      // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
+      ps.keywordSearch( searchedKeyword, placesSearchCB); 
     }
-  }
+  
+    // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
+    function placesSearchCB(data, status) {
+  
+      if (status === kakao.maps.services.Status.OK) {
+        console.log(data)
+      
+      } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+          alert('검색 결과가 존재하지 않습니다.');
+          return;
+      } else if (status === kakao.maps.services.Status.ERROR) {
+          alert('검색 결과 중 오류가 발생했습니다.');
+          return;
+      }
+    }
+
+  }, [searchedKeyword])
 
 
   return(
