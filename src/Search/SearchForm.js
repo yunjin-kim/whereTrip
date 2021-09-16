1cf09edaa1fe4802a0e8115d3786e406b85c5d61
(yunjin) <sion11290805@gmail.com>
2021-09-16 21:42:57 +0900

    검색페이지css

--- src/Search/SearchForm.js
+++ src/Search/SearchForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import '../css/SearchForm.css';
 import {InputGroup, FormControl, Button } from 'react-bootstrap'
 
 const SearchForm = ({searchKeyword}) => {
@@ -17,15 +18,16 @@ const SearchForm = ({searchKeyword}) => {
 
   return(
     <>
-      <form onSubmit={searchSubmit}>
+      <form onSubmit={searchSubmit} className="searchForm">
         <InputGroup className="mb-3" >
           <FormControl 
+            className="searchInput"
             onChange={searchInput}
             placeholder="어디로 떠날까요?"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
           />
-          <Button variant="outline-secondary" id="button-addon2" type="submit">
+          <Button className="searchBtn" variant="outline-secondary" id="button-addon2" type="submit">
             떠나기
           </Button>
         </InputGroup>
--- src/Search/SearchResult.js
+++ src/Search/SearchResult.js
@@ -9,7 +9,7 @@ const SearchResult = ({searchData}) => {
       <div className="resultWrap">
       {
         searchData.map((data)=>(
-          <Card className="searchResultCard" style={{ width: '18rem' }}>
+          <Card className="searchResultCard " style={{ width: '18rem' }}>
             <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
               <Card.Body>
                 <Card.Link href="{data.place_url}">{data.place_name}</Card.Link>
