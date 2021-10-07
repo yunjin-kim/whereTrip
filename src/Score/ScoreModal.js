8cc08b41148c2c20e4dca48b03c351ef3bed9dde
(yunjin) <sion11290805@gmail.com>
2021-10-08 08:21:45 +0900

    검색결과 오류 수정

--- src/Score/ScoreModal.js
+++ src/Score/ScoreModal.js
@@ -10,58 +10,6 @@ const ScoreModal = ({setScoreModal}) => {
     setScoreModal(false);
   }
 
-
-  // const [oneStar, setOneStar] = useState(false);
-  // const [twoStar, setTwoStar] = useState(false);
-  // const [threeStar, setThreeStar] = useState(false);
-  // const [fourStar, setFourStar] = useState(false);
-  // const [fiveStar, setFiveStar] = useState(false);
-
-  // const ClickOneStar = () => {
-  //   setPostScore(1);
-  //   setOneStar(true);
-  //   setTwoStar(false);
-  //   setThreeStar(false);
-  //   setFourStar(false);
-  //   setFiveStar(false);
-  // }
-
-  // const ClickTwoStar = () => {
-  //   setPostScore(2);
-  //   setOneStar(true);
-  //   setTwoStar(true);
-  //   setThreeStar(false);
-  //   setFourStar(false);
-  //   setFiveStar(false);
-  // }
-
-  // const ClickThreeStar = () => {
-  //   setPostScore(3);
-  //   setOneStar(true);
-  //   setTwoStar(true);
-  //   setThreeStar(true);
-  //   setFourStar(false);
-  //   setFiveStar(false);
-  // }
-
-  // const ClickFourStar = () => {
-  //   setPostScore(4);
-  //   setOneStar(true);
-  //   setTwoStar(true);
-  //   setThreeStar(true);
-  //   setFourStar(true);
-  //   setFiveStar(false);
-  // }
-
-  // const ClickFiveStar = () => {
-  //   setPostScore(5);
-  //   setOneStar(true);
-  //   setTwoStar(true);
-  //   setThreeStar(true);
-  //   setFourStar(true);
-  //   setFiveStar(true);
-  // }
-
   let starArr = [];
   const [clickStar, setClickStar] = useState(starArr);
 
@@ -83,11 +31,6 @@ const ScoreModal = ({setScoreModal}) => {
       <div className="scoreModal">
         <button className="scoreModalClose" onClick={scoreModalClose}>X</button>
         <div>
-          {/* <FontAwesomeIcon icon={faStar} onClick={ClickOneStar} className={classnames('clickOneStar',{clickedOneStar : oneStar})} />
-          <FontAwesomeIcon icon={faStar} onClick={ClickTwoStar} className={classnames('clickTwoStar',{clickedTwoStar : twoStar})}   />
-          <FontAwesomeIcon icon={faStar} onClick={ClickThreeStar} className={classnames('clickThreeStar',{clickedThreeStar : threeStar})}   />
-          <FontAwesomeIcon icon={faStar} onClick={ClickFourStar} className={classnames('clickFourStar',{clickedFourStar : fourStar})}   />
-          <FontAwesomeIcon icon={faStar} onClick={ClickFiveStar} className={classnames('clickFiveStar',{clickedFiveStar : fiveStar})}   /> */}
           <FontAwesomeIcon icon={faStar} onClick={()=>clickStarOnOff(0)} className={clickStar[0]} />
           <FontAwesomeIcon icon={faStar} onClick={()=>clickStarOnOff(1)} className={clickStar[1]}   />
           <FontAwesomeIcon icon={faStar} onClick={()=>clickStarOnOff(2)} className={clickStar[2]}   />
--- src/Search/SearchResult.js
+++ src/Search/SearchResult.js
@@ -11,12 +11,14 @@ const SearchResult = React.memo(({searchData}) => {
     setScoreModal(true)
   },[scoreModal])
 //후기 작성 버튼 누르면 후기id에 검색결과 id를 넣어서 후기 작성완료하면 DB에서 구분
+//키 값을 배열의 인덱스로한 이유는 만약 검색결과의 id로 한다면 가끔 카카오 검색결과를 불러오면 같은 검색결과 15개가 두번씩 불러와질 때가 있다 
+//그럴 때 검색결과의 id로 한다면 key값이 겹치기 때문에 오류가 발생한다 배열의 인덱스로 하면 그런 오류를 방지할 수 있다 검색결과를 재배열 하지는 않기 때문에 배열의 인덱스를 키값으로 하는것이 좋은 선택?
   return(
     <>
       <div className="resultWrap">
         {
-          searchData.map((data)=>(
-            <Card className="searchResultCard" key={data.id} style={{ width: '18rem' }}>
+          searchData.map((data, idx)=>(
+            <Card className="searchResultCard" key={idx} style={{ width: '18rem' }}>
               {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
                 <Card.Body>
                   <a href={`https://place.map.kakao.com/${data.id}`} target="_blank">
