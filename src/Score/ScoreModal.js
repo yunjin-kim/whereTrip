b57ddc9ff873af5ca4005a6f6aef7cf0799e7180
(yunjin) <sion11290805@gmail.com>
2021-10-08 21:16:01 +0900

    별점클릭하면점수

--- src/Score/ScoreModal.js
+++ src/Score/ScoreModal.js
@@ -1,28 +1,46 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import '../css/ScoreModal.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-import classnames from 'classnames';
 
 const ScoreModal = ({setScoreModal}) => {
-  const [postScore, setPostScore] = useState(0);
+  let [postScore, setPostScore] = useState(0);
+  const [clickStar, setClickStar] = useState('');
+
   const scoreModalClose = () => {
     setScoreModal(false);
   }
 
-  let starArr = [];
-  const [clickStar, setClickStar] = useState(starArr);
+  console.log(postScore)
 
-  function clickStarOnOff(idx){
+
+  // useEffect(() => {
+  //   clickStar.map((star)=>{
+  //     if(star === "yesClick"){
+  //       setPostScore(postScore++);
+  //     }
+  //     else{
+  //       setPostScore(postScore--);
+  //     }
+  //   })
+  // }, [setClickStar])
+
+
+
+  function clickStarScroe(idx){
+    setPostScore(0)
+    let starNum = 0;
     let starClickArr = [];
     for(let i = 0; i < 5; i++){
       if(i <= idx){
         starClickArr.push('yesClick')
+        starNum++;
       }
       else{
         starClickArr.push('noClick')
       }
     }
+    setPostScore(starNum)
     setClickStar(starClickArr)
   }
 
@@ -31,11 +49,11 @@ const ScoreModal = ({setScoreModal}) => {
       <div className="scoreModal">
         <button className="scoreModalClose" onClick={scoreModalClose}>X</button>
         <div>
-          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarOnOff(0)} className={clickStar[0]} />
-          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarOnOff(1)} className={clickStar[1]}   />
-          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarOnOff(2)} className={clickStar[2]}   />
-          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarOnOff(3)} className={clickStar[3]}   />
-          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarOnOff(4)} className={clickStar[4]}   />
+          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarScroe(0)} className={clickStar[0]} />
+          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarScroe(1)} className={clickStar[1]}   />
+          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarScroe(2)} className={clickStar[2]}   />
+          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarScroe(3)} className={clickStar[3]}   />
+          <FontAwesomeIcon icon={faStar} onClick={()=>clickStarScroe(4)} className={clickStar[4]}   />
         </div>
         <textarea></textarea>
         <button>작성</button>
